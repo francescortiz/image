dc000f928064ff084643ba6afd6d5d13fa97bad3
Francesc Ortiz <francescoriz@gmail.com>
2012-04-22 12:13:12 +0200

    update license to BSD

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='image',
-    version='1.0',
+    version='1.1',
     author=u'Francesc Ortiz',
     author_email='francescortiz@gmail.com',
     packages=['image'],
     url='https://github.com/francescortiz/image',
-    license='GPLv3',
+    license='BSD',
     description='Django application that provides cropping, resizing, ' + \
 		'thumbnailing, overlays and masking for images and videos with ' + \
 		'the ability to set the center of attention,',

f9e486bef803cd2d5de9b93ebce9a5dd44dae542
Francesc Ortiz <francescoriz@gmail.com>
2012-05-19 21:55:21 +0200

    add a function to be able to create image links from code more easily

---
+++ image/misc.py
@@ -0,0 +1,19 @@
+#-*- coding: UTF-8 -*-
+
+from image.views import image as image_view
+from django.core.urlresolvers import reverse
+from image.utils import image_create_token
+
+def get_image_url(image, parameters):
+    if 'autogen=true' in parameters:
+        print "CACA " + image_view(None, str(image), parameters, True)
+    
+    return reverse(
+        'image.views.image',
+        args=(
+            unicode(image),
+            image_create_token(parameters)
+        )
+    )
+
+

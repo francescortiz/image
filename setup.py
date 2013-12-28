3fc3515121f614dad47376e6f794acad3e9f2d3d
Francesc Ortiz <francescortiz@gmail.com>
2013-12-28 18:09:43 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='image',
     version='1.1',
     author=u'Francesc Ortiz',
     author_email='francescortiz@gmail.com',
-    packages=['image'],
+    packages=find_packages(),
     url='https://github.com/francescortiz/image',
     license='BSD',
     description='Django application that provides cropping, resizing, ' + \
-		'thumbnailing, overlays and masking for images and videos with ' + \
-		'the ability to set the center of attention,',
+                'thumbnailing, overlays and masking for images and videos with ' + \
+                'the ability to set the center of attention,',
     long_description='Django application that provides cropping, resizing, ' + \
-		'thumbnailing, overlays and masking for images and videos with ' + \
-		'the ability to set the center of attention,',
+                     'thumbnailing, overlays and masking for images and videos with ' + \
+                     'the ability to set the center of attention,',
     zip_safe=False,
     include_package_data=True,
+    install_requires=[
+        'pillow',
+    ],
 )

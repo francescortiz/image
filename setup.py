7d6da419673386dc0499cddbc01978d2d1d6c85b
Francesc Ortiz <francescortiz@gmail.com>
2014-05-28 16:36:08 +0200

    - increase version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='image',
-    version='1.3.1',
+    version='1.3.2',
     author=u'Francesc Ortiz',
     author_email='francescortiz@gmail.com',
     packages=find_packages(),

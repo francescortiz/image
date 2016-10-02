9529270fb20eff8978cac0712862ed527086774b
Francesc Ortiz <francescortiz@gmail.com>
2016-10-02 12:26:41 +0200

    It seems that django 1.10 requires urls to have a name if you want to be able to reverse them.

--- image/urls.py
+++ image/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from image import views
 
 urlpatterns = [
-    url(r'^crosshair$', views.crosshair),
-    url(r'^(?P<path>.+)/(?P<token>[\w_=&]+)$', views.image),
+    url(r'^crosshair$', views.crosshair, name="image.views.crosshair"),
+    url(r'^(?P<path>.+)/(?P<token>[\w_=&]+)$', views.image, name="image.views.image"),
 ]
